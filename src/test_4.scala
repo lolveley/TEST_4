261da738878d8ab7ba55c191cf2039b5d9a5eb86
olivier <lolveley@laposte.net>
2013-05-03 22:58:11 +0200

    2e mais 1er vrai commit

---
+++ src/test_4.scala
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: olivier
+ * Date: 03/05/13
+ * Time: 22:55
+ * To change this template use File | Settings | File Templates.
+ */
+object test_4{
+
+  def main (args:Array[String]){
+
+    println("salut")
+  }
+
+}
